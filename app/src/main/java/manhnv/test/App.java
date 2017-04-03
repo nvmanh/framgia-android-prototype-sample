de50436dbee618b4570edd20b47362bf549ae466
root <root@framgia0100-pc.framgia.com>
2017-04-03 09:32:34 +0700

    initialize project

---
+++ app/src/main/java/manhnv/test/App.java
@@ -0,0 +1,21 @@
+package manhnv.test;
+
+import android.app.Application;
+
+/**
+ * Created by root on 3/31/17.
+ */
+
+public class App extends Application {
+    private static App _self;
+
+    public static App self() {
+        return _self;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        _self = this;
+    }
+}
